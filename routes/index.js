9f8e52635e5f2b8e5e6fd8ca6deff412716b38c8
Alexandre Dotor Casals <adotorc@uoc.edu>
2015-07-17 14:21:18 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -25,6 +26,8 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+              sessionController.loginRequired, commentController.publish);
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
